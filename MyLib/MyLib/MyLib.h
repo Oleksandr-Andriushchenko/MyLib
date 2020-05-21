a754156373242e3e1f5ab820b7d41f8222d5c39d
Alexander Andriushchenko <>
2020-05-21 14:49:17 +0300

    Inital project

---
+++ MyLib/MyLib/MyLib.h
@@ -0,0 +1,19 @@
+//
+//  MyLib.h
+//  MyLib
+//
+//  Created by Alexander Andriushchenko on 21.05.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLib.
+FOUNDATION_EXPORT double MyLibVersionNumber;
+
+//! Project version string for MyLib.
+FOUNDATION_EXPORT const unsigned char MyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLib/PublicHeader.h>
+
+
---
+++ MyLib/MyLib/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  MyLib
+//
+//  Created by Alexander Andriushchenko on 21.05.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+
+    static func subBigBoy() -> String {
+        return "What's up BIG BOY!?"
+    }
+}
